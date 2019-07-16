753227f6902d94c9ebb03d1fba451e01cadfcf0a
2665025934@qq.com <2665025934@qq.com>
2019-07-16 15:42:13 +0800

    教师管理

--- application/index/controller/Teacher.php
+++ application/index/controller/Teacher.php
@@ -13,6 +13,33 @@ namespace app\index\controller;
 class Teacher extends BaseController
 {
     /**
+     * 我的查询，用于搜索
+     */
+    protected function _where($model){
+        if(!$model){
+            return '';
+        }
+        $teacher_name = input('get.teacher_name');
+        $status = input('get.status/d');
+        $se_id = input('get.se_id/d');
+        $status?$model->where('status',$status):'';
+        $se_id?$model->where('se_id',$se_id):'';
+        $teacher_name?$model->whereLike('t_name','%'.$teacher_name.'%'):'';
+
+        return $model;
+    }
+    /**
+     * 教师列表
+     */
+    public function get(){
+       $model = \app\index\model\Teacher
+            ::field('t_id as id,t_name as name,sex,cellphone,birthday,entry_time,status,se_id')
+            ->order('create_time desc');
+       $res = $this->_where($model)->paginate(20);
+
+       $this->return_data(1,0,'',$res);
+    }
+    /**
      * 新增教师
      */
     public function add(){
@@ -73,5 +100,40 @@ class Teacher extends BaseController
         }catch (\Exception $e){
             $this->return_data(0,50000,$e->getMessage());
         }
+    }
+
+    /**
+     * 设置某些字段，如离职
+     */
+    public function set_field(){
+        $id = input('id/d');
+        $field = input('field');
+        $action = input('action');
+        $data = [
+            't_id'=>$id,
+            'field'=>$field,
+            'action'=>$action
+        ];
+        $validate = new \app\index\validate\Teacher();
+        if(!$validate->scene('field')->check($data)){
+            //为了可以得到错误码
+            $error = explode('|',$validate->getError());
+            $this->return_data(0,$error[1],$error[0]);
+        }
+        $sinfo = '';
+        try{
+            switch ($field){
+                case 1://离职
+                    $field = 'status';
+                    $sinfo = '离职成功';
+                    break;
+            }
+            \app\index\model\Teacher::where('t_id',$id)->update([$field=>$action]);
+            $this->return_data(1,0,$sinfo);
+        }catch (\Exception $e){
+            $this->return_data(0,50000,$e->getMessage());
+        }
+
+
     }
 }
--- application/index/model/Teacher.php
+++ application/index/model/Teacher.php
@@ -15,7 +15,7 @@ class Teacher extends BaseModel
     protected $field = true;
     protected $autoWriteTimestamp = true;
     protected $auto = ['manager','entry_time'];//操作人id，对应users表主键
-
+    protected $append = ['seniority_name'];
     protected $insert = ['status'=>2];
     protected $update = [];
     protected function setManagerAttr(){
@@ -42,6 +42,16 @@ class Teacher extends BaseModel
         }else{//合法格式
             return strtotime($data);
         }
+    }
+
+    //资历
+     function getSeniorityNameAttr($value,$data){
+        return Seniorities::where('seniority_id',$data['se_id'])->value('seniority_name');
+    }
+
+    //关联资历表
+    protected function seniority(){
+        return $this->hasOne('Seniorities','seniority_id','se_id');
     }
 
 
