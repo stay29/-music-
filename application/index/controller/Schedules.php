a34d071de3cf753f9d4ce1b01e6bdd2970bbf9ca
kjoyc <kjoyc@126.com>
2019-09-05 11:34:20 +0800

    l

--- application/index/controller/Schedules.php
+++ application/index/controller/Schedules.php
@@ -2,15 +2,15 @@
 
 
 namespace app\index\controller;
-
+use app\index\model\Schedule;
 
 class Schedules extends BaseController
 {
     public function add_teacher_schedule(){
+
 //        $data=[
 //            ''
-//        ]
-//        return
+//        ]     return
     }
 
 }
---
+++ application/index/model/Schedule.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace app\index\model;
+
+
+class Schedule extends BaseModel
+{
+    protected $table = 'erp2_teach_schedules';
+
+}
