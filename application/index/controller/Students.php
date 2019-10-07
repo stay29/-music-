cabc6ddbbcba9c4ccba1da325a34edef5add62ee
zhouzhihao <378623234@qq.com>
2019-10-07 17:52:34 +0800

    'l'

--- application/index/controller/Students.php
+++ application/index/controller/Students.php
@@ -486,7 +486,7 @@ class Students extends BaseController
         }catch (Exception $e)
         {
             Db::rollback();
-            $this->return_data('0', '50000', '服务器错误');
+            $this->return_data('0', '50000', $e->getMessage());
         }
     }
 
