a0ad062c107c3ef4744657ce5f16ed08d0d256db
kjoyc <kjoyc@126.com>
2019-10-12 17:31:00 +0800

    增加签到模块

---
+++ application/index/controller/Attend.php
@@ -0,0 +1,27 @@
+<?php
+namespace app\index\controller;
+
+use app\index\controller\BaseController;
+use think\Exception;
+
+/*
+ * 员工考勤管理
+ */
+class Attend extends BaseController
+{
+    //员工考勤情况
+    public function index(){
+        
+    }
+    
+    //打卡
+    public function mark_down(){
+        
+    }
+    
+    //学生打卡
+    public function todo(){
+        
+    }
+}
+
