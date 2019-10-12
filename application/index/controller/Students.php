c0e90a1642cc2af9d98281816d02e09498a22273
zhouzhihao <378623234@qq.com>
2019-10-12 17:25:40 +0800

    l

--- application/index/controller/Students.php
+++ application/index/controller/Students.php
@@ -655,10 +655,11 @@ class Students extends BaseController
         $stu_id=input('stu_id');
      $data=Db::name('stu_balance')->where('stu_id',$stu_id)
          ->select();
-     $data[0]['details']=Db::field('a.amount,a.presenter,b.payment_method,a.balance,a.remark,a.create_time')
+     $data[0]['details']=Db::field('c.truename,a.amount,a.presenter,b.payment_method,a.balance,a.remark,a.create_time,a.type')
          ->name('stu_balance_log')
          ->alias('a')
          ->join('erp2_payments b','a.pay_id=b.pay_id')
+         ->join('erp2_students c',['c.stu_id'=>$stu_id])
          ->where('pid',$stu_id)
          ->order('a.create_time desc')
          ->select();
